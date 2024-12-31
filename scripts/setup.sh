f792ab9e1e6452f3bda4e237ffd7b717b64c5222
Diogo Guedes <diogoguedes@Air-de-Familia.Home>
2024-12-31 12:46:03 +0000

    update dotfiles

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,10 +1,17 @@
 # Deploy fzf
 
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+#git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+#fzf/install
 
 # Startship install
 
-brew install starship
+#brew install starship
+
+
+
+# Symbolic links
+
+ln -sf "$DOTFILES/bash/.bashrc" ~/.bashrc
+ln -sf "$DOTFILES/bash/.bash_profile" ~/.bash_profile
 
 
