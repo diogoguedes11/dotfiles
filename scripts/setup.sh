a356e28880293cf9fd0d0620aa044042439bc2a5
Diogo Guedes <diogoguedes@Air-de-Familia.Home>
2024-12-31 15:46:39 +0000

    Updates dotfiles

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,18 +1,52 @@
-# Deploy fzf
+#!/bin/bash
 
-#git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-#fzf/install
+DOTFILES_DIR="$HOME/Repos/github.com/diogobytes/dotfiles"
+XDG_CONFIG_HOME="$HOME/.config"
 
-# Startship install
+create_directories() {
+  local directories=("$@")
+  for dir in "${directories[@]}"; do
+    mkdir -p "$dir"
+  done
+}
 
-#brew install starship
+create_symlinks() {
+  local items=("$@")
+  for item in "${items[@]}"; do
+    IFS=':' read -r source target <<<"$item"
+    if [ -L "$target" ]; then
+      echo "Removing existing symlink $target"
+      unlink "$target"
+    elif [ -d "$target" ]; then
+      echo "Warning: $target is a directory. Skipping..."
+      continue
+    elif [ -e "$target" ]; then
+      echo "Warning: $target already exists. Skipping..."
+      continue
+    fi
+    ln -s "$DOTFILES_DIR/$source" "$target"
+    echo "Created symlink for $source"
+  done
+}
 
+common_directories=(
+  "$XDG_CONFIG_HOME/alacritty"
+  "$XDG_CONFIG_HOME/alacritty/themes"
+  "$XDG_CONFIG_HOME/skhd"
+  "$XDG_CONFIG_HOME/k9s"
+)
 
+common_items=(
+  "alacritty.toml:$XDG_CONFIG_HOME/alacritty/alacritty.toml"
+  "k9s/skin.yml:$XDG_CONFIG_HOME/k9s/skin.yml"
+  ".inputrc:$HOME/.inputrc"
+  ".tmux.conf:$HOME/.tmux.conf"
+  "nvim:$XDG_CONFIG_HOME/nvim"
+  "skhdrc:$XDG_CONFIG_HOME/skhd/skhdrc"
+  ".zprofile:$HOME/.zprofile"
+  ".zshrc:$HOME/.zshrc"
+)
 
-# Symbolic links
-
-ln -sf "$DOTFILES/bash/.bashrc" ~/.bashrc
-ln -sf "$DOTFILES/bash/.bash_profile" ~/.bash_profile
-ln -sf .inputrc ~/.inputrc
-
+create_directories "${common_directories[@]}"
+create_symlinks "${common_items[@]}"
 
