310b24e39aa1cdbf9e8a0f65103342a899ff241f
Diogo Guedes <diogo.guedes111@gmail.com>
2025-01-14 20:13:44 +0100

    chore: update dotfiles

--- scripts/apps.sh
+++ scripts/apps.sh
@@ -5,7 +5,26 @@
 MACOS_INFO=$OSTYPE
 
 if [[ -z $MACOS_INFO ]]; then
-  echo "Executing installer for Ubuntu..."
+  echo "Executing installer for Linux..."
+  # requirements
+  sudo apt-get install build-essential procps curl file git
+
+  # brew installation
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+  # Install fzf
+  git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+  ~/.fzf/install
+
+  # kubernetes
+  curl -lo "https://dl.k8s.io/release/$(curl -l -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+  sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+  # tmux
+  apt install tmux
+  # neovim
+  snap install nvim --classic
+  # k9s
+  brew install derailed/k9s/k9s
 
 else
   echo "Installing in MacOS"
@@ -24,7 +43,3 @@ else
   brew install koekeishiya/formulae/skhd
   skhd --start-service
 fi
-
-# Kubernetes
-   curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
- sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
